75d59b3e4f097742970cb2a090768f35cc84bec8
Flippylolz <assasin463@gmail.com>
2017-01-21 01:09:13 +0200

    comments deleted

--- 3.rb
+++ 3.rb
@@ -1,7 +1,6 @@
 class Vibor
   def sel
     h={'yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234' , :yesss => :fg, try: 30, key: 'some value', 'yesterday1' => 34, 'yesteryear' => 2014}
-
     h.select {|k| k =~ /\byes\w*/}.size
   end
 end
