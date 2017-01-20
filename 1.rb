60b30c3071c0698ea88e4a56d16521a59abb3745
Flippylolz <assasin463@gmail.com>
2017-01-21 00:37:59 +0200

    1 line prog

--- 1.rb
+++ 1.rb
@@ -1,2 +1,2 @@
-a = Array.new(3){Array.new(3){rand(-10..10)}}
-p a.flatten.select {|a| a > 0}.inject(:+)
+p a = Array.new(3){Array.new(3){rand(-10..10)}}.flatten.select {|a| a > 0}.inject(:+)
+# p a.flatten.select {|a| a > 0}.inject(:+)
