d99c72decdbecf46971ae6c61e799e55c7991e6d
Flippylolz <assasin463@gmail.com>
2017-01-21 00:46:43 +0200

    fix

--- 1.rb
+++ 1.rb
@@ -1,2 +1,2 @@
-p a = Array.new(3){Array.new(3){rand(-10..10)}}.flatten.select {|a| a > 0}.inject(:+)
-# p a.flatten.select {|a| a > 0}.inject(:+)
+a = Array.new(3){ Array.new(3){ rand(-10..10) } }
+p a.flatten.select { |a| a > 0 }.inject(:+)
