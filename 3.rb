98bb482a50da50981aa626933a01c08337c19206
Flippylolz <assasin463@gmail.com>
2017-01-21 00:25:30 +0200

    comments deleted

--- 3.rb
+++ 3.rb
@@ -1,8 +1,4 @@
 class Vibor
-  # attr_accessor :some1
-  #def initialize (some)
-  # @@some = some#  встоений метод которий принимает параметри для класса
-  #end
   def sel
     h={'yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234' , :yesss => :fg, try: 30, key: 'some value', 'yesterday1' => 34, 'yesteryear' => 2014}
 
@@ -12,12 +8,3 @@ end
 
 yes=Vibor.new
 p yes.sel
-
-#yes=Vibor.new({'yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234' , :yesss => :fg, try: 30, key: 'some value', 'yesterday1' => 34, 'yesteryear' => 2014})
-#p yes.sel
-#yes1=Vibor.new({'yes' => 23,'yas' => 12,'heg' => :f})
-#p yes1.sel
-#yes.some1 = "Hey" #setter
-#p yes.some1 #getter
-#yes1=Vibor.new({'yes' => 1,'yas' => 2,'kek' =>'???'})
-#p yes1.sel
