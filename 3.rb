120c2a99983fdcfd64da81c1b5a65d77ea6c7d81
Flippylolz <asasines@ukr.net>
2017-01-20 19:34:49 +0200

    hot fix

--- 3.rb
+++ 3.rb
@@ -1,15 +1,22 @@
 class Vibor
-  #attr_accessor :some1
-  def initialize (some)
-    @some = some#  встоений метод которий принимает параметри для класса
-  end
+  # attr_accessor :some1
+  #def initialize (some)
+  # @@some = some#  встоений метод которий принимает параметри для класса
+  #end
   def sel
-    @some.select {|k| k =~ /\byes\w*/}.size
+    h={'yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234' , :yesss => :fg, try: 30, key: 'some value', 'yesterday1' => 34, 'yesteryear' => 2014}
+
+    h.select {|k| k =~ /\byes\w*/}.size
   end
 end
 
-yes=Vibor.new({'yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234' , :yesss => :fg, try: 30, key: 'some value', 'yesterday1' => 34, 'yesteryear' => 2014})
+yes=Vibor.new
 p yes.sel
+
+#yes=Vibor.new({'yes' => 23, 'b' => 'travel', 'yesterday' => 34, 5 => '234' , :yesss => :fg, try: 30, key: 'some value', 'yesterday1' => 34, 'yesteryear' => 2014})
+#p yes.sel
+#yes1=Vibor.new({'yes' => 23,'yas' => 12,'heg' => :f})
+#p yes1.sel
 #yes.some1 = "Hey" #setter
 #p yes.some1 #getter
 #yes1=Vibor.new({'yes' => 1,'yas' => 2,'kek' =>'???'})
