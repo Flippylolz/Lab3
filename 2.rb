4b26e763b65e82fea883ba268b3d04e62c0941e2
Flippylolz <assasin463@gmail.com>
2017-03-01 15:16:02 +0200

    Case sensitivity fixed

--- 2.rb
+++ 2.rb
@@ -1,4 +1,4 @@
 h = {'yes' => 23, 'b' => 'travel', 'yesterday' => 34,
      5 => '234', :yesss => :fg, try: 30, key: 'some value',
      'yesterday1' => 34, 'yesteryear' => 2014}
-p h.select {|k| k =~ /\byes\w*/ }.size
+p h.select {|k| k =~ /\byes\w*/i }.size
